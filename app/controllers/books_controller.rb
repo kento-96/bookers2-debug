2afcf2d2565e4bf789558ba390e502172bdf129c
kento-96 <kento.soccer.1107@gmail.com>
2021-05-02 01:34:39 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,24 @@
 class BooksController < ApplicationController
+ before_action :authenticate_user!
 
   def show
+    @new = Book.new
     @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def index
     @books = Book.all
+    @user = User.find(current_user.id)
+    @book=Book.new
   end
 
   def create
     @book = Book.new(book_params)
+    @user=User.find(current_user.id)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
@@ -21,6 +27,11 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render"edit"
+    else
+      redirect_to books_path
+    end
   end
 
 
@@ -28,22 +39,22 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
       render "edit"
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
 
 end
